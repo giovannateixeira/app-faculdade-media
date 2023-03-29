6d8a856f2a081f347d7e3003405c6da6b5235846
Giovanna Teixeira <83679417+giovannateixeira@users.noreply.github.com>
2023-03-28 21:16:32 -0300

    Update index.js

--- src/components/Form/Result/index.js
+++ src/components/Form/Result/index.js
@@ -2,11 +2,19 @@ import {Text, View} from 'react-native';
 import styles from './style';
 
 export default function Result(props) {
+    let color
+    if(props.media < 3){
+        color = styles.corResprovado
+    }else if(props.media >= 3 && props.media < 7){
+        color = styles.corProvaFinal
+    }else{
+        color = styles.corAprovado
+    }
     return(
         <View style={styles.resultadoMedia}>
             <Text style={styles.info}>Sua média foi:</Text>
             <Text style={styles.media}>{props.media}</Text>
-            <Text style={[styles.msgResultado, styles.corReprovado]}>{props.msgResultado}</Text>
+            <Text style={[styles.msgResultado, color]}>{props.msgResultado}</Text>
         </View>
     );
 }
